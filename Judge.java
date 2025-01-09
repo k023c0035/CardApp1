ce91407a45fae7d415a64eee6a1fa98fe5b51daa
k023c0035 <k023c0035@g.neec.ac.jp>
2025-01-09 14:18:55 +0900

    Update Judge.java

--- Judge.java
+++ Judge.java
@@ -7,7 +7,7 @@ public class Judge{
            case "J": return 11;
            case "Q": return 12;
            case "K": return 13;
-           case "A": return 14;
+           case "A": return 1;
            default: return Integer.parseInt(stCard); 
        }
    }
